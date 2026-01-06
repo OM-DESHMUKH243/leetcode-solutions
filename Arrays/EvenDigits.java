a3bc8e60119bdb10fa167ca9ddd1337631a71523
OM-DESHMUKH243 <jayudeshmukh243@Mac.lan>
2026-01-06 19:45:48 +0530

    Day 6 Solved Even Digit Numbers (Easy)

---
+++ Arrays/EvenDigits.java
@@ -0,0 +1,24 @@
+class EvenDigits {
+
+    public int findEvenDigitNumbers(int[] nums) {
+
+        int count = 0;
+
+        for (int num : nums) {
+
+            int digits = 0;
+            int temp = num;
+
+            while (temp != 0) {
+                digits++;
+                temp = temp / 10;
+            }
+
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
