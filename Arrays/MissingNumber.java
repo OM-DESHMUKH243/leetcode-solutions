d4bad4813b0466731ae19e2ffd5bd8e05c6171bd
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-01-11 23:04:30 +0530

    Missing Number

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,18 @@
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+
+        // XOR all numbers from 0 to n
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+
+        // XOR all elements in the array
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
