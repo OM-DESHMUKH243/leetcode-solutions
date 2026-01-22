d593cb96fdd820cdd91939087c6f2b177b63f426
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-01-22 23:28:04 +0530

    Two Sum II (Two Pointers)

---
+++ Arrays/TwoSumII.java
@@ -0,0 +1,21 @@
+class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                // return 1-based indices
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{}; // problem guarantees a solution
+    }
+}
