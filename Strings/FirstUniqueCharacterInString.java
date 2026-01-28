ba04fc2712a22f734f9c997d84e8a86c6bb668e6
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-01-28 23:49:11 +0530

    First Unique Character in a String

---
+++ Strings/FirstUniqueCharacterInString.java
@@ -0,0 +1,21 @@
+package Strings;
+
+class FirstUniqueCharacterInString {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        // Count frequency
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Find first unique character
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
