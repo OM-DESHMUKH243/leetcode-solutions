17342e36736e2a28a87a910617208bd09163034d
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-02-03 23:48:47 +0530

    Linked Link Cycle II

---
+++ LinkedList/LinkedListCycleII.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode entry = head;
+                while (entry != slow) {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+}
