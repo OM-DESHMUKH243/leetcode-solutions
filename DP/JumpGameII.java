c35898cbc268e8b44627782271cf0d52f76fcda1
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-03-23 23:21:27 +0530

    Jump Game (Greedy + DP)

---
+++ DP/JumpGameII.java
@@ -0,0 +1,35 @@
+package DP;
+
+public class JumpGameII {
+
+    public static int jump(int[] nums){
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for(int i = 0; i < nums.length - 1; i++){
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if(i == currentEnd){
+
+                jumps++;
+                currentEnd = farthest;
+
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args){
+
+        int[] nums = {2,3,1,1,4};
+
+        int result = jump(nums);
+
+        System.out.println("Minimum jumps: " + result);
+
+    }
+}
