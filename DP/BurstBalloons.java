1a5d16b27eb9e9928e581904368130b767375cd4
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-04-14 22:39:23 +0530

    Interval DP / Partition DP

---
+++ DP/BurstBalloons.java
@@ -0,0 +1,48 @@
+package DP;
+
+public class BurstBalloons {
+
+    public static int maxCoins(int[] nums){
+
+        int n = nums.length;
+
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+
+        for(int i = 0; i < n; i++){
+            arr[i + 1] = nums[i];
+        }
+
+        int[][] dp = new int[n + 2][n + 2];
+
+        for(int len = 2; len < n + 2; len++){
+
+            for(int left = 0; left + len < n + 2; left++){
+
+                int right = left + len;
+
+                for(int k = left + 1; k < right; k++){
+
+                    dp[left][right] = Math.max(
+                        dp[left][right],
+                        arr[left] * arr[k] * arr[right]
+                        + dp[left][k]
+                        + dp[k][right]
+                    );
+                }
+            }
+        }
+
+        return dp[0][n + 1];
+    }
+
+    public static void main(String[] args){
+
+        int[] nums = {3,1,5,8};
+
+        int result = maxCoins(nums);
+
+        System.out.println("Maximum Coins: " + result);
+    }
+}
