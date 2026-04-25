8c6adbeedb5356a6ed7a97ede64beea49a4dbd66
Om Deshmukh <omdeshmukh4455@gmail.com>
2026-04-25 23:33:41 +0530

    KMP / Prefix Function

---
+++ Strings/LongestHappyPrefix.java
@@ -0,0 +1,47 @@
+package Strings;
+
+public class LongestHappyPrefix {
+
+    public static String longestPrefix(String s){
+
+        int n = s.length();
+
+        int[] lps = new int[n];
+
+        int len = 0;
+        int i = 1;
+
+        while(i < n){
+
+            if(s.charAt(i) == s.charAt(len)){
+
+                len++;
+                lps[i] = len;
+                i++;
+
+            } else {
+
+                if(len != 0){
+
+                    len = lps[len - 1];
+
+                } else {
+
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return s.substring(0, lps[n - 1]);
+    }
+
+    public static void main(String[] args){
+
+        String s = "ababab";
+
+        String result = longestPrefix(s);
+
+        System.out.println("Longest Happy Prefix: " + result);
+    }
+}
